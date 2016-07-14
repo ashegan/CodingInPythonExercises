b7f03ee6d041e265301a08ea03c27ef95469869e
ashegan <ashegan2@gmail.com>
2016-07-14 14:18:14 -0400

    sum_ane.py
    Interactive sum script

---
+++ sum_ane.py
@@ -0,0 +1,7 @@
+"""
+Asked to write a script that asks the user for two numbers then adds them together and gives a response."""
+
+num1 = int(input('Choose a number: '))
+num2 = int(input ('Choose another number: '))
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
